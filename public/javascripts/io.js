8203ccb24a6de01150d390878b7a4ed7c17a28cd
3Dn <3Dn@live.ru>
2016-03-01 12:01:13 +0300

    all for live

--- public/javascripts/io.js
+++ public/javascripts/io.js
@@ -5,3 +5,22 @@ socket.on('on_connect', function(data){
 });
 
 socket.emit('ready', 'q');
+
+function ws_start(webSocketServer){
+    esp_ws = new WebSocket(webSocketServer);
+    esp_ws.onopen = function(){
+        console.log("ESP Ok!");
+        esp_ws.send("Hi!");
+    }
+    esp_ws.onmessage = function(event){
+        //console.log("Data: " + event.data);
+        document.querySelector('#socket_text').innerHTML = "Data: " + event.data;
+    }
+    esp_ws.onclose = function(){
+        setTimeout(function(){
+            ws_start(webSocketServer)
+        }, 5000);
+    }
+}
+
+ws_start("ws://192.168.1.78:81");
